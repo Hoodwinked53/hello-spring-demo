c28eb375f79856571cb4bf4229e1b6eb57d35f4c
Hoodwinked53 <78771132+Hoodwinked53@users.noreply.github.com>
2021-06-06 14:36:37 -0500

    Finished exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,90 +1,60 @@
 package org.launchcode.hellospring.controllers;
 
-import org.apache.coyote.Request;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-    // Handles request at path /hello
+    //    // Handle request at path /hello
 //    @GetMapping("hello")
 //    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
-    // lives at /hello/goodbye due to above RequestMapping
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    // lives at /hello/hello due to above RequestMapping
     // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
-    // lives at /hello/form
+
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                    "<body>" +
-                        "<form action = 'hello' method = 'post'>" + // submit a request to /hello
-                            "<input type = 'text' name = 'name'>" +
-                            "<input type = 'submit' value = 'Greet me!'>" +
-                        "</form>" +
-                    "</body>" +
-                "</html>";
+        return "form";
     }
 
-    @RequestMapping(value = "greeting", method = RequestMethod.GET)
-    public String greeting() {
-        return "<form method = 'post'>" +
-                "<input type = 'text' name = 'name'>" +
-                "<select name = 'language'>" +
-                "<option value = 'english'> English </option>" +
-                "<option value = 'french'> French </option>" +
-                "<option value = 'italian'> Italian </option>" +
-                "<option value = 'spanish'> Spanish </option>" +
-                "<option value = 'german'> German </option>" +
-                "</select>" +
-                "<input type = 'submit' value = 'Greet Me!' />" +
-                "</form>";
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
-
-    @RequestMapping(value = "greeting", method = RequestMethod.POST)
-    public static String createMessage(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
-
-        String message = "";
-
-        if (language.equals("english")) {
-            message = "Hello, ";
-        } else if (language.equals("french")) {
-            message = "Bonjour, ";
-        } else if (language.equals("italian")) {
-            message = "Ciao, ";
-        } else if (language.equals("spanish")) {
-            message = "Hola, ";
-        } else if (language.equals("german")) {
-            message = "Hallo, ";
-        }
-
-        return "<p style = 'color: red; text-align: center; margin-top: 20vh'>" + message + name + "</p>";
-    }
-
 
 }
